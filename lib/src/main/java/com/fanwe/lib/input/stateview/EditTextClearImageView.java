8e51ddb45f1597c626e426e1e03c29350713223e
zjun <565061763@qq.com>
2018-06-22 17:37:39 +0800

    注释

--- lib/src/main/java/com/fanwe/lib/input/stateview/EditTextClearImageView.java
+++ lib/src/main/java/com/fanwe/lib/input/stateview/EditTextClearImageView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2017 zhengjun, fanwe (http://www.fanwe.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fanwe.lib.input.stateview;
 
 import android.content.Context;
@@ -10,7 +25,7 @@ import com.fanwe.lib.input.FEditText;
 import com.fanwe.lib.input.R;
 
 /**
- * Created by zhengjun on 2018/4/4.
+ * 清除输入框内容
  */
 public class EditTextClearImageView extends ImageView implements FEditText.StateView
 {
