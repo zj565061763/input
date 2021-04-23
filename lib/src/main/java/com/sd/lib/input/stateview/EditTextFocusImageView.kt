360c392b668cc45f6a4fc86bbde372eb149f200e
zjun <565061763@qq.com>
2021-04-23 17:15:50 +0800

    EditTextFocusImageView kotlin

--- lib/src/main/java/com/sd/lib/input/stateview/EditTextFocusImageView.kt
+++ lib/src/main/java/com/sd/lib/input/stateview/EditTextFocusImageView.kt
@@ -1,40 +1,31 @@
-package com.sd.lib.input.stateview;
+package com.sd.lib.input.stateview
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.widget.EditText;
-import android.widget.ImageView;
-
-import com.sd.lib.input.FEditTextContainer;
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+import android.widget.ImageView
+import com.sd.lib.input.FEditTextContainer
 
 /**
  * 焦点变化，切换选中状态
  */
-public class EditTextFocusImageView extends ImageView implements FEditTextContainer.StateView
-{
-    public EditTextFocusImageView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
+class EditTextFocusImageView : ImageView, FEditTextContainer.StateView {
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun setVisibility(visibility: Int) {
+        if (getVisibility() != visibility) {
+            super.setVisibility(visibility)
+        }
     }
 
-    @Override
-    public void setVisibility(int visibility)
-    {
-        if (getVisibility() != visibility)
-            super.setVisibility(visibility);
+    override fun setSelected(selected: Boolean) {
+        if (isSelected != selected) {
+            super.setSelected(selected)
+        }
     }
 
-    @Override
-    public void setSelected(boolean selected)
-    {
-        if (isSelected() != selected)
-            super.setSelected(selected);
-    }
-
-    @Override
-    public void onUpdate(EditText editText)
-    {
-        setSelected(editText.isFocused());
-        setVisibility(editText.getVisibility());
+    override fun onUpdate(editText: EditText) {
+        isSelected = editText.isFocused
+        visibility = editText.visibility
     }
 }
