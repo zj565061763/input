b05c0f74b4f1517e68e6440f5fae2fd1288b718d
zjun <565061763@qq.com>
2021-04-23 17:08:50 +0800

    EditTextClearImageView kotlin

--- lib/src/main/java/com/sd/lib/input/stateview/EditTextClearImageView.kt
+++ lib/src/main/java/com/sd/lib/input/stateview/EditTextClearImageView.kt
@@ -1,74 +1,50 @@
-package com.sd.lib.input.stateview;
+package com.sd.lib.input.stateview
 
-import android.content.Context;
-import android.util.AttributeSet;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-
-import com.sd.lib.input.FEditTextContainer;
-import com.sd.lib.input.R;
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.EditText
+import android.widget.ImageView
+import com.sd.lib.input.FEditTextContainer.StateView
+import com.sd.lib.input.R
 
 /**
  * 清除输入框内容
  */
-public class EditTextClearImageView extends ImageView implements FEditTextContainer.StateView
-{
-    public EditTextClearImageView(Context context, AttributeSet attrs)
-    {
-        super(context, attrs);
-        init();
+class EditTextClearImageView : ImageView, StateView {
+    private var _editText: EditText? = null
+    private var _onClickListener: OnClickListener? = null
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        if (drawable == null) {
+            setImageResource(R.drawable.lib_input_selector_edit_clear)
+        }
+
+        super.setOnClickListener { v ->
+            _editText?.setText("")
+            _onClickListener?.onClick(v)
+        }
     }
 
-    private EditText mEditText;
-    private View.OnClickListener mOnClickListener;
-
-    private void init()
-    {
-        if (getDrawable() == null)
-            setImageResource(R.drawable.lib_input_selector_edit_clear);
-
-        super.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                if (mEditText != null)
-                    mEditText.setText("");
-
-                if (mOnClickListener != null)
-                    mOnClickListener.onClick(v);
-            }
-        });
+    override fun setOnClickListener(l: OnClickListener) {
+        _onClickListener = l
     }
 
-    @Override
-    public void setOnClickListener(View.OnClickListener l)
-    {
-        mOnClickListener = l;
+    override fun setVisibility(visibility: Int) {
+        if (getVisibility() != visibility) {
+            super.setVisibility(visibility)
+        }
     }
 
-    @Override
-    public void setVisibility(int visibility)
-    {
-        if (getVisibility() != visibility)
-            super.setVisibility(visibility);
-    }
-
-    @Override
-    public void onUpdate(EditText editText)
-    {
-        mEditText = editText;
-
-        if (editText.getVisibility() == View.VISIBLE
-                && editText.isFocused()
-                && editText.isEnabled()
-                && editText.getText().length() > 0)
-        {
-            setVisibility(VISIBLE);
-        } else
-        {
-            setVisibility(GONE);
+    override fun onUpdate(editText: EditText) {
+        _editText = editText
+        if (editText.visibility == VISIBLE &&
+                editText.isFocused &&
+                editText.isEnabled &&
+                editText.text.isNotEmpty()
+        ) {
+            this.visibility = VISIBLE
+        } else {
+            this.visibility = GONE
         }
     }
 }
