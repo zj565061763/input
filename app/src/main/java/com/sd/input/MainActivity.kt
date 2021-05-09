82f8409eff7083733786b550b7f5d3ab776ba538
zjun <565061763@qq.com>
2021-05-09 23:06:33 +0800

    demo

--- app/src/main/java/com/sd/input/MainActivity.kt
+++ app/src/main/java/com/sd/input/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.sd.input;
+package com.sd.input
 
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-public class MainActivity extends AppCompatActivity
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
